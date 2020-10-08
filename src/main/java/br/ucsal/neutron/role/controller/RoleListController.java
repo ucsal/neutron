10acc90c6373558428681263c486fe19e16d95b6
Luis <luisfelipe.tecnologias@gmail.com>
2020-10-08 15:34:27 -0300

    #5 ROLE - Criação das classes e servlets necessários, precisa ser testado e trabalhar no form.

---
+++ src/main/java/br/ucsal/neutron/role/controller/RoleListController.java
@@ -0,0 +1,31 @@
+package br.ucsal.neutron.role.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.ucsal.neutron.role.dao.RoleDAO;
+import br.ucsal.neutron.role.model.Role;
+
+
+
+@WebServlet("/rolelist")
+public class RoleListController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   private RoleDAO dao = new RoleDAO();
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Role> roles = dao.listar();
+		
+		request.setAttribute("roles", roles);
+		request.getRequestDispatcher("./role/list.jsp").forward(request, response);
+	}
+
+}
