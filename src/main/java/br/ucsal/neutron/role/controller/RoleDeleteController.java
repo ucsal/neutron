8bbbd27326ebf9344a689842bc8f4726baac44e8
Luis <luisfelipe.tecnologias@gmail.com>
2020-10-08 15:16:19 -0300

    #5 Role - Criação das classes e servlets necessários, restam RoleListController e RoleDeleteController.

---
+++ src/main/java/br/ucsal/neutron/role/controller/RoleDeleteController.java
@@ -0,0 +1,41 @@
+package br.ucsal.neutron.role.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RoleDeleteController
+ */
+@WebServlet("/RoleDeleteController")
+public class RoleDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RoleDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/br/ucsal/neutron/role/controller/RoleSaveController.java
@@ -0,0 +1,37 @@
+package br.ucsal.neutron.role.controller;
+
+import java.io.IOException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.ucsal.neutron.role.dao.RoleDAO;
+import br.ucsal.neutron.role.model.Role;
+
+@WebServlet("/RoleSaveController")
+public class RoleSaveController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RoleDAO dao = new RoleDAO();
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+
+		Role role = new Role(name, description);
+		dao.salvar(role);
+		request.getSession().setAttribute("salve_sucess", true);
+
+		response.sendRedirect("./role");
+		
+	
+	}
+
+}
